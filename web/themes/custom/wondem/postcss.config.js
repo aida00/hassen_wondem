2d51b394510558e02a6bc73e422638653ef86955
aida00 <admin@oype.com>
2025-09-12 19:27:24 -0600

    updated site and  work on site layout

--- web/themes/custom/wondem/postcss.config.js
+++ web/themes/custom/wondem/postcss.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// BEFORE (broken)
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+
+// AFTER (working with Vite ESM)
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
 };
